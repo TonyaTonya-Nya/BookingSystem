e69f34bee14ca91ecffadd8cf12d0414357bd85d
吳俊漳 <a0979790220@gmail.com>
2020-10-13 02:48:16 +0800

    Add:DB module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './userDB/user.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsController } from './cats.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
+    
+  controllers: [AppController,CatsController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/userDB/user.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { UsersService } from './user.service';
+import { User } from './user.entity';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) { }
+
+  @Get()
+  findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+}
